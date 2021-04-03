d0f023ca089f5c8d7af0209443c29693205d9c0c
Marchenko_Stepan <electronic11@outlook.com>
2021-04-03 22:44:27 +0700

    DE is ready to suppurted theme installations

--- .bin/install-script.sh
+++ .bin/install-script.sh
@@ -8,25 +8,12 @@ cd polybar
 makepkg -si --skipinteg --noconfirm
 cd ..
 rm -rf polybar
-git clone https://github.com/vinceliuice/Qogir-theme
-cd Qogir-theme
-./install.sh -t standard -l arch -c dark
-cd ..
-rm -rf Qogir-theme
-git clone https://github.com/vinceliuice/Qogir-icon-theme
-cd Qogir-icon-theme
-./install.sh
-cd ..
-rm -rf Qogir-icon-theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd $ZSH/custom/plugins
 git clone https://github.com/zdharma/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions
 cd $HOME
-mkdir Documents && mkdir Downloads && mkdir Images
-mv --target-directory=$HOME/Images $HOME/Wallpapers
 mv .zshrc-cybernetic .zshrc
-mv --target-directory=$HOME/.icons $HOME/.local/share/icons/*
 sudo mv xorg.conf.d /etc/X11
 git clone https://aur.archlinux.org/urxvt-clipboard.git
 cd urxvt-clipboard
