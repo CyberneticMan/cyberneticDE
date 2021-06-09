72126acf5901ed15cfe752cfac31abf2bd8e59ac
Marchenko_Stepan <electronic11@outlook.com>
2021-06-09 15:26:32 +0700

    Update scripts

--- .bin/tmrun.sh
+++ .bin/tmrun.sh
@@ -11,13 +11,26 @@ else
   #
   tmux new -d -s $SESSION
   #
-  tmux new-window -t $SESSION:1 -n 'admin'
+  tmux new-window -t $SESSION:1 -n 'Admin'
   tmux split-window -h
   tmux select-pane -t 1
-  tmux resize-pane -R 10
+  tmux resize-pane -R 5
   tmux split-window -v
   tmux resize-pane -D 1
-  tmux select-pane -t 3
+  tmux send-keys "htop" C-m
+  tmux select-pane -t 0
+  tmux send-keys "ranger" C-m
+  tmux select-pane -t 1
+  #
+  tmux new-window -t $SESSION:2 -n 'Edit'
+  tmux send-keys "nvim" C-m
+  tmux split-window -h
+  tmux resize-pane -R 40
+  tmux select-pane -t 1
+  tmux send-keys C-m
+  #
+  tmux new-window -t $SESSION:3 -n 'Web-browser'
+  tmux send-keys "w3m https://duckduckgo.com" C-m
   #
   tmux select-window -t $SESSION:1
   tmux attach-session -t $SESSION
